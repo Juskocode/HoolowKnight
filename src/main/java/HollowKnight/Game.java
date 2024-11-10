fb68ffe273c469b6a93b31ee711a01fd8130cbd9
Freitas <adavidaf2002@gmail.com>
2024-11-10 12:56:13 +0000

    Fixed main Game class, synced controller with states

--- src/main/java/HollowKnight/Game.java
+++ src/main/java/HollowKnight/Game.java
@@ -6,20 +6,27 @@ import HollowKnight.model.menu.Menu;
 import HollowKnight.state.MenuState;
 import HollowKnight.state.State;
 
+import java.awt.*;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Game {
     private final LanternaGUI gui;
     private State state;
 
-    public Game(LanternaGUI gui) throws IOException {
+    public Game() throws IOException, URISyntaxException, FontFormatException {
         this.gui = new LanternaGUI(80, 40);
         this.state = new MenuState(new Menu());
     }
 
-    public static void main(String[] args) throws IOException{
-            Game game = new Game(new LanternaGUI());
-            game.start();
+    public static void main(String[] args) throws IOException, URISyntaxException, FontFormatException {        Logger logger = Logger.getLogger(Game.class.getName());
+        try {
+            new Game().start();
+        } catch (Exception e) {
+            logger.log(Level.INFO, "An error occurred", e);
+        }
     }
 
     public void setState(MenuState state) {
@@ -30,7 +37,7 @@ public class Game {
         int FPS = 60;
         int TBF = 1000/60;
 
-        while (this.state.isRuning()) {
+        while (this.state != null) {
             long startTime = System.currentTimeMillis();
             state.move(this, gui, startTime);
             long elapsedTime = System.currentTimeMillis() - startTime;
--- src/main/java/HollowKnight/view/states/MenuViewer.java
+++ src/main/java/HollowKnight/view/states/MenuViewer.java
@@ -1,6 +1,7 @@
 package HollowKnight.view.states;
 
 import HollowKnight.gui.GUI;
+import HollowKnight.model.menu.Menu;
 
 public class MenuViewer extends ScreenViewer<Menu> {
     public MenuViewer(GUI gui, Menu model) {
