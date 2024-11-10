f4d9db01882258b128e2fcc8cbc8e74dfe5994e8
Freitas <adavidaf2002@gmail.com>
2024-11-10 11:44:41 +0000

    Added initial state viewers

--- src/main/java/HollowKnight/view/states/GameViewer.java
+++ src/main/java/HollowKnight/view/states/GameViewer.java
@@ -1,4 +1,15 @@
 package HollowKnight.view.states;
 
-public class GameViewer {
+import HollowKnight.gui.GUI;
+import HollowKnight.model.game.elements.map.Map;
+
+public class GameViewer extends ScreenViewer<Map> {
+    public GameViewer(GUI gui, Map map) {
+        super(gui, map);
+    }
+
+    @Override
+    public void draw() {
+
+    }
 }
--- src/main/java/HollowKnight/view/states/MenuViewer.java
+++ src/main/java/HollowKnight/view/states/MenuViewer.java
@@ -1,4 +1,15 @@
 package HollowKnight.view.states;
 
-public class MenuViewer {
+import HollowKnight.controller.menu.Menu;
+import HollowKnight.gui.GUI;
+
+public class MenuViewer extends ScreenViewer<Menu> {
+    public MenuViewer(GUI gui, Menu model) {
+        super(gui, model);
+    }
+
+    @Override
+    public void draw() {
+
+    }
 }
