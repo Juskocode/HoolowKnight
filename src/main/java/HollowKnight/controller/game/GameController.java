22c810b7b07404a636262d6a7afe7990955005a4
juskocode <adavidaf2002@gmail.com>
2024-11-12 23:15:13 +0000

    removed GameController.java

--- src/main/java/HollowKnight/controller/game/GameController.java
+++
@@ -1,8 +0,0 @@
-package HollowKnight.controller.game;
-
-import HollowKnight.controller.Controller;
-import HollowKnight.model.game.scene.Scene;
-
-public abstract class GameController extends Controller<Scene> {
-    public GameController(Scene scene) {super(scene);}
-}
