4ebf7859a7bec1792fea5942b0241d0321179c72
juskocode <adavidaf2002@gmail.com>
2024-11-12 23:25:44 +0000

    Updated Viewers regarding Player Sprite

--- src/main/java/HollowKnight/view/elements/KnightViewer.java
+++ src/main/java/HollowKnight/view/elements/KnightViewer.java
@@ -4,8 +4,6 @@ import HollowKnight.gui.GUI;
 
 import HollowKnight.model.game.elements.Knight.Knight;
 import HollowKnight.view.sprites.Sprite;
-import HollowKnight.view.sprites.SpriteLoader;
-import com.googlecode.lanterna.TextColor;
 
 import java.io.IOException;
 
@@ -13,7 +11,7 @@ import java.io.IOException;
 public class KnightViewer implements ElementViewer<Knight>{
     @Override
     public void draw(Knight model, GUI gui) throws IOException {
-        Sprite sprite = new SpriteLoader().createSprite("sprites/Knight/Idle/pixil-frame-0.png");
+        Sprite sprite = new Sprite("sprites/Knight/Idle/pixil-frame-0.png");
         sprite.draw(gui, model.getPosition().x(), model.getPosition().y());
     }
     /*
