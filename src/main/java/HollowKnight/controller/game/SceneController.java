9404c56ba8679c4638a1d36a9684f896fc5fe9b2
frutastontas <joaopedrofurtado2005@gmail.com>
2024-11-12 22:58:33 +0000

    Added particles and it's controller on scene and randomized the behaviour for particles

--- src/main/java/HollowKnight/controller/game/SceneController.java
+++ src/main/java/HollowKnight/controller/game/SceneController.java
@@ -4,19 +4,24 @@ import HollowKnight.Game;
 import HollowKnight.gui.GUI;
 import HollowKnight.model.game.scene.Scene;
 
+import java.io.IOException;
+
 public class SceneController extends GameController {
     private final PlayerController playerController;
+    private final ParticleController particleController;
 
-    public SceneController(Scene scene, PlayerController playerController) {
+    public SceneController(Scene scene, PlayerController playerController, ParticleController particleController) {
         super(scene);
         this.playerController = playerController;
+        this.particleController = particleController;
     }
 
-    public void move(Game game, GUI.ACTION action, long time){
+    public void move(Game game, GUI.ACTION action, long time) throws IOException {
         if (action == GUI.ACTION.QUIT)
             game.setState(null);
         else {
             playerController.move(game, action, time);
+            particleController.move(game,action,time);
         }
     }
 }
