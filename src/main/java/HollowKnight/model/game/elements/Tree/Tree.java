19f200b9558358476678a6aea13baf49588521ca
juskocode <adavidaf2002@gmail.com>
2024-11-13 01:24:23 +0000

    Added Tree.java Element

---
+++ src/main/java/HollowKnight/model/game/elements/Tree/Tree.java
@@ -0,0 +1,9 @@
+package HollowKnight.model.game.elements.Tree;
+
+import HollowKnight.model.game.elements.Element;
+
+public class Tree extends Element {
+    public Tree(int x, int y) {
+        super(x, y);
+    }
+}
