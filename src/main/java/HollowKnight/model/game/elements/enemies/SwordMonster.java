62d10b1fa4a8fde9992791718ecf30439ab3d213
juskocode <adavidaf2002@gmail.com>
2024-11-13 15:19:27 +0000

    Added SwordMonster.java and SwordMonsterViewer.java

---
+++ src/main/java/HollowKnight/model/game/elements/enemies/SwordMonster.java
@@ -0,0 +1,9 @@
+package HollowKnight.model.game.elements.enemies;
+
+import HollowKnight.model.game.elements.Element;
+
+public class SwordMonster extends Element {
+    public SwordMonster(int x, int y) {
+        super(x, y);
+    }
+}
