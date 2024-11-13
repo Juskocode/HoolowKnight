22267b35a4f9267656a9791710acd07ab6e2f74f
juskocode <adavidaf2002@gmail.com>
2024-11-13 01:23:59 +0000

    Added BigRock.java element

---
+++ src/main/java/HollowKnight/model/game/elements/rocks/BigRock.java
@@ -0,0 +1,7 @@
+package HollowKnight.model.game.elements.rocks;
+
+import HollowKnight.model.game.elements.Element;
+
+public class BigRock extends Element {
+    public BigRock(int x, int y) {super(x, y);}
+}
