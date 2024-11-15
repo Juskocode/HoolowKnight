7c962b2acdf0e0f4f9326f38637bf85a1cfff113
Freitas <adavidaf2002@gmail.com>
2024-11-15 18:31:55 +0000

    Updated OptionsViewer for Text support

--- src/main/java/HollowKnight/view/menu/OptionViewer.java
+++ src/main/java/HollowKnight/view/menu/OptionViewer.java
@@ -2,23 +2,20 @@ package HollowKnight.view.menu;
 
 import HollowKnight.gui.GUI;
 import HollowKnight.model.menu.Option;
+import HollowKnight.view.text.GameTextViewer;
+import HollowKnight.view.text.TextViewer;
 import com.googlecode.lanterna.TextColor;
 
 import java.io.IOException;
 
 public class OptionViewer {
+    private final TextViewer textViewer;
+
+    public OptionViewer() throws IOException{
+        this.textViewer = new GameTextViewer();
+    }
     public void draw(Option model, GUI gui, TextColor.RGB color) throws IOException {
-        for (int oidx = 0; oidx < 8; oidx++){
-            for (int idx = 0; idx < 6; idx++) {
-                gui.drawPixel((int)model.getPosition().x() + idx + oidx*8, (int)model.getPosition().y(), color);
-                gui.drawPixel((int)model.getPosition().x() + idx + oidx*8, (int)model.getPosition().y() + 1, color);
-                gui.drawPixel((int)model.getPosition().x() + idx + oidx*8, (int)model.getPosition().y() + 2, color);
-                gui.drawPixel((int)model.getPosition().x() + idx + oidx*8, (int)model.getPosition().y() + 3, color);
-                gui.drawPixel((int)model.getPosition().x() + idx + oidx*8, (int)model.getPosition().y() + 4, color);
-                gui.drawPixel((int)model.getPosition().x() + idx + oidx*8, (int)model.getPosition().y() + 5, color);
-                gui.drawPixel((int)model.getPosition().x() + idx + oidx*8, (int)model.getPosition().y() + 6, color);
-                gui.drawPixel((int)model.getPosition().x() + idx + oidx*8, (int)model.getPosition().y() + 7, color);
-            }
-        }
+        textViewer.draw(model.getText(), (int)model.getPosition().x(), (int)model.getPosition().y(), color, gui);
+
     }
 }
