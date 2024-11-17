b65a1eb935020b9bda2401f16a83e2d8a6a2d08e
juskocode <adavidaf2002@gmail.com>
2024-11-17 22:11:51 +0000

    Added FallingState and updated all states according to the statemachine

--- src/main/java/HollowKnight/model/game/elements/Knight/IdleState.java
+++ src/main/java/HollowKnight/model/game/elements/Knight/IdleState.java
@@ -14,12 +14,13 @@ public class IdleState extends KnightState {
                 velocity.x() * getKnight().getAcceleration(),
                 velocity.y()
         );
-        return limitVelocity(newVelocity);
+        return limitVelocity(applyCollisions(newVelocity));
     }
 
     @Override
     public KnightState getNextState() {
-
+        if (!getKnight().isOnGround())
+            return getNextOnAirState();
         if (Math.abs(getKnight().getVelocity().x()) >= WalkingState.MIN_VELOCITY)
             return new WalkingState(getKnight());
         return this;
