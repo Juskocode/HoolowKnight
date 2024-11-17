5ff081acdcf83ec1765366ffdc222b8153e48665
juskocode <adavidaf2002@gmail.com>
2024-11-17 04:23:37 +0000

    Added walking state between idle and walking

--- src/main/java/HollowKnight/model/game/elements/Knight/WalkingState.java
+++ src/main/java/HollowKnight/model/game/elements/Knight/WalkingState.java
@@ -3,19 +3,27 @@ package HollowKnight.model.game.elements.Knight;
 import HollowKnight.model.Vector;
 
 public class WalkingState extends KnightState{
-    public static double MIN_Velocity = 0.1;
+    public static double MIN_VELOCITY = 0.75;
 
     public WalkingState(Knight knight) {
         super(knight);
     }
 
     @Override
-    public Vector updateVelocity(Vector newVelocity) {
-        return null;
+    public Vector updateVelocity(Vector velocity) {
+        Vector newVelocity = new Vector(
+                velocity.x() * getKnight().getAcceleration(),
+                velocity.y()
+        );
+        return limitVelocity(newVelocity);
     }
 
     @Override
     public KnightState getNextState() {
-        return null;
+        if (Math.abs(getKnight().getVelocity().x()) >= RunningState.MIN_VELOCITY)
+            return new RunningState(getKnight());
+        if (Math.abs(getKnight().getVelocity().x()) < WalkingState.MIN_VELOCITY)
+            return new IdleState(getKnight());
+        return this;
     }
 }
