6ab46c1ae174868dec59255463c2f5b33b3f87de
juskocode <adavidaf2002@gmail.com>
2024-11-17 22:12:07 +0000

    Set TILE_SIZE on tile.java

--- src/main/java/HollowKnight/model/game/elements/tile/Tile.java
+++ src/main/java/HollowKnight/model/game/elements/tile/Tile.java
@@ -3,6 +3,8 @@ package HollowKnight.model.game.elements.tile;
 import HollowKnight.model.game.elements.Element;
 
 public class Tile extends Element {
+    public static final int SIZE = 8;
+
     public Tile(int x, int y) {
         super(x, y);
     }
