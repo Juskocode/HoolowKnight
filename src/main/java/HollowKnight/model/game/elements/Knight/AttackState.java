9f57bee7306b255190942f781fa9d1b45f2d1f8c
juoum00000 <jeosantos2005@gmail.com>
2024-11-22 16:45:32 +0000

    Implemented jump into AttackState

--- src/main/java/HollowKnight/model/game/elements/Knight/AttackState.java
+++ src/main/java/HollowKnight/model/game/elements/Knight/AttackState.java
@@ -1,4 +1,5 @@
 package HollowKnight.model.game.elements.Knight;
+import HollowKnight.model.Vector;
 
 public abstract class AttackState extends KnightState {
     public AttackState(Knight knight) {
@@ -6,6 +7,22 @@ public abstract class AttackState extends KnightState {
     }
 
     public abstract void Attack();
+
+
+
+    @Override
+    public Vector jump(){
+
+        getKnight().setJumpCounter(getKnight().getJumpCounter() + 1);
+        Vector newVelocity = new Vector(
+            getKnight().getVelocity().x(),
+            getKnight().getVelocity().y() - getKnight().getJumpBoost()
+        );
+
+        return updateVelocity(newVelocity);
+
+    }
+
 
 
 
