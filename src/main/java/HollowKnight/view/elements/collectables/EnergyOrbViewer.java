58d271f7de2f7152f289e9172583297ba24b7649
juskocode <adavidaf2002@gmail.com>
2024-11-23 19:34:52 +0000

    Refactored Viewer of Collectables

--- src/main/java/HollowKnight/view/elements/collectables/EnergyOrbViewer.java
+++ src/main/java/HollowKnight/view/elements/collectables/EnergyOrbViewer.java
@@ -12,7 +12,7 @@ public class EnergyOrbViewer implements ElementViewer<EnergyOrb> {
     private final Sprite energyOrbSprite;
 
     public EnergyOrbViewer() throws IOException {
-        this.energyOrbSprite = new Sprite("sprites/Collectables/energy.png");
+        this.energyOrbSprite = new Sprite("sprites/collectables/energy.png");
     }
     @Override
     public void draw(EnergyOrb model, GUI gui, long time) throws IOException {
