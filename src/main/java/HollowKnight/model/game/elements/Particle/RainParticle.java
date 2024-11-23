e92173bff68b94dafbecd7e9e14ed855fbcfe08d
juskocode <adavidaf2002@gmail.com>
2024-11-23 19:45:25 +0000

    ALL particles implement collision, particles engine

--- src/main/java/HollowKnight/model/game/elements/Particle/RainParticle.java
+++ src/main/java/HollowKnight/model/game/elements/Particle/RainParticle.java
@@ -1,13 +1,38 @@
 package HollowKnight.model.game.elements.Particle;
 
 import HollowKnight.model.Position;
+import HollowKnight.model.Vector;
 import HollowKnight.model.game.scene.Scene;
 import com.googlecode.lanterna.TextColor;
+
+import static java.lang.Math.max;
 
 public class RainParticle extends Particle {
 
     public RainParticle(int x, int y, Position velocity, TextColor.RGB color) {
         super(x, y, velocity, color);
+    }
+
+    @Override
+    protected Vector applyCollisions(Vector velocity)
+    {
+        double x = getPosition().x(), y = getPosition().y();
+        double vx = velocity.x(), vy = velocity.y();
+        Position knightSize = new Position(1, 1);
+
+        while (vy > 0 && getScene().collidesDown(new Position(x, y + vy), knightSize))
+            vy = Math.max(vy - 1, 0);
+
+        while (vy < 0 && getScene().collidesUp(new Position(x, y + vy), knightSize))
+            vy = Math.min(vy + 1, 0);
+
+        while (vx < 0 && getScene().collidesLeft(new Position(x + vx, y + vy), knightSize))
+            vx = Math.min(vx + 1, 0);
+
+        while (vx > 0 && getScene().collidesRight(new Position(x + vx, y + vy), knightSize))
+            vx = max(vx - 1, 0);
+
+        return new Vector(vx, vy);
     }
 
     @Override
