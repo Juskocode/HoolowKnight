a9daf110789a69087a2f0d1b50d7ac952fa04976
juskocode <adavidaf2002@gmail.com>
2024-11-23 19:44:54 +0000

    PlayerController debugging feature for respawning player

--- src/main/java/HollowKnight/controller/game/PlayerController.java
+++ src/main/java/HollowKnight/controller/game/PlayerController.java
@@ -3,22 +3,21 @@ package HollowKnight.controller.game;
 import HollowKnight.Game;
 import HollowKnight.controller.Controller;
 import HollowKnight.gui.GUI;
-import HollowKnight.model.Position;
-import HollowKnight.model.Vector;
 import HollowKnight.model.game.elements.Knight.IdleState;
 import HollowKnight.model.game.elements.Knight.Knight;
+import HollowKnight.model.game.elements.Knight.RespawnState;
 import HollowKnight.model.game.scene.Scene;
 
 public class PlayerController extends Controller<Scene> {
     public PlayerController(Scene scene) {
         super(scene);
     }
-  
+
     @Override
     public void move(Game game, GUI.ACTION action, long time) {
         Knight knight = getModel().getPlayer();
 
-        switch (action){
+        switch (action) {
             case LEFT:
                 knight.setVelocity(knight.moveLeft());
                 knight.setFacingRight(false);
@@ -28,8 +27,25 @@ public class PlayerController extends Controller<Scene> {
                 knight.setFacingRight(true);
                 break;
             case JUMP:
+                /*
+                if (gui.isJumpHeld()) { // Process jump only if the key is being held
+
+                    Vector velocity = knight.jump();
+                    long duration = gui.getDuration();
+
+                    // Clamp duration between 80 and 250
+                    if (duration > 250) duration = 250;
+                    else if (duration < 80) duration = 80;
+
+                    double jumpMultiplier = duration / 250.0;
+                    System.out.println("JumpBoost " + jumpMultiplier);
+
+                    knight.setVelocity(velocity);
+                }*/
                 knight.setVelocity(knight.jump());
                 break;
+            case KILL:
+                knight.setState(new RespawnState(knight, 30));
             default:
                 knight.setVelocity(knight.updateVelocity());
         }
@@ -42,4 +58,5 @@ public class PlayerController extends Controller<Scene> {
             knight.setState(new IdleState(knight));
         }
     }
+
 }
