774249e048c7a010ddb06b208c404f3e8fd59b6d
juskocode <adavidaf2002@gmail.com>
2024-11-24 00:05:05 +0000

    ParticleController cleanup

--- src/main/java/HollowKnight/controller/game/ParticleController.java
+++ src/main/java/HollowKnight/controller/game/ParticleController.java
@@ -18,24 +18,20 @@ public class ParticleController extends Controller<Scene>{
     public void move(Game game, GUI.ACTION action, long time) throws IOException {
         for(Particle particle: getModel().getParticles()){
             particle.setScene(getModel());
-            System.out.println("rain " + particle.getScene().getClass().getSimpleName());
             particle.setPosition(particle.moveParticle(getModel(), time));
 
         }
         for(Particle particle: getModel().getDoubleJumpParticles()){
             particle.setScene(getModel());
-            System.out.println("Double jump " + particle.getScene().getClass().getSimpleName());
             particle.setPosition(particle.moveParticle(getModel(), time));
 
         }
         for(Particle particle: getModel().getJumpParticles()){
             particle.setScene(getModel());
-            System.out.println("jump " + particle.getScene().getClass().getSimpleName());
             particle.setPosition(particle.moveParticle(getModel(), time));
         }
         for(Particle particle: getModel().getRespawnParticles()){
             particle.setScene(getModel());
-            System.out.println("blood " + particle.getScene().getClass().getSimpleName());
             particle.setPosition(particle.moveParticle(getModel(), time));
         }
     }
