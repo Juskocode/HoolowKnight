252b20cddf80157eeb38160ac09e0c9679308de2
juskocode <adavidaf2002@gmail.com>
2024-11-25 00:49:35 +0000

    Added tests for LR and Sr attacks refactored Attack state

--- src/main/java/HollowKnight/model/game/elements/Knight/AttackState.java
+++ src/main/java/HollowKnight/model/game/elements/Knight/AttackState.java
@@ -1,29 +1,4 @@
 package HollowKnight.model.game.elements.Knight;
-import HollowKnight.model.Vector;
 
-public abstract class AttackState extends KnightState {
-    public AttackState(Knight knight) {
-        super(knight);
-    }
-
-    public abstract void Attack();
-
-
-
-    @Override
-    public Vector jump(){
-
-        getKnight().setJumpCounter(getKnight().getJumpCounter() + 1);
-        Vector newVelocity = new Vector(
-            getKnight().getVelocity().x(),
-            getKnight().getVelocity().y() - getKnight().getJumpBoost()
-        );
-
-        return updateVelocity(newVelocity);
-
-    }
-
-
-
-
+public class AttackState {
 }
