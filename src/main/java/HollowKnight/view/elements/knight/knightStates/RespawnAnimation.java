d3ab4a4281b5497a5523ec084c462291392120f9
Freitas <adavidaf2002@gmail.com>
2024-11-26 12:05:27 +0000

    RespawnAnimation implementation for state pattern

---
+++ src/main/java/HollowKnight/view/elements/knight/knightStates/RespawnAnimation.java
@@ -0,0 +1,16 @@
+package HollowKnight.view.elements.knight.knightStates;
+
+import HollowKnight.view.elements.PairList;
+
+public class RespawnAnimation extends StateAnimation{
+    public RespawnAnimation(Class state, int frames) {
+        super(state, frames);
+    }
+
+    @Override
+    protected void loadAnimation(String path) {
+        setState(RunningAnimation.class);
+        setAnimation(new PairList<>(null, null));
+        setFrames(1);
+    }
+}
