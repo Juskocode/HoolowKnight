2b089ff28bc8fcd90efa361663f296a1136013af
juskocode <adavidaf2002@gmail.com>
2024-11-26 21:38:56 +0000

    Added after and during dash state place older animation

--- src/main/java/HollowKnight/view/elements/knight/knightStates/AfterDashAnimation.java
+++ src/main/java/HollowKnight/view/elements/knight/knightStates/AfterDashAnimation.java
@@ -1,8 +1,11 @@
 package HollowKnight.view.elements.knight.knightStates;
 
 import HollowKnight.view.elements.PairList;
+import HollowKnight.view.sprites.Sprite;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AfterDashAnimation extends StateAnimation{
     public AfterDashAnimation(Class state, int frames) {
@@ -11,8 +14,14 @@ public class AfterDashAnimation extends StateAnimation{
 
     @Override
     public void loadAnimation(String path) throws IOException {
+        List<Sprite> runningSpriteRight = new ArrayList<>();
+        List<Sprite> runningSpriteLeft = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            runningSpriteRight.add(new Sprite(path + "/movement/running/running-final-" + i + ".png"));
+            runningSpriteLeft.add(new Sprite(path + "/movement/running/running-final-" + i + "-reversed.png"));
+        }
         setState(AfterDashAnimation.class);
-        setAnimation(new PairList<>(null, null));
-        setFrames(1);
+        setAnimation(new PairList<>(runningSpriteLeft, runningSpriteRight));
+        setFrames(5);
     }
 }
