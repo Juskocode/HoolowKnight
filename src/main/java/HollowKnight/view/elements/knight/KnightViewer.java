e2d779f1c10c7da475149f2fc57ac731e92abe03
juskocode <adavidaf2002@gmail.com>
2024-11-26 21:31:33 +0000

    Refactored into state pattern

--- src/main/java/HollowKnight/view/elements/knight/KnightViewer.java
+++ src/main/java/HollowKnight/view/elements/knight/KnightViewer.java
@@ -6,6 +6,7 @@ import HollowKnight.model.game.elements.Knight.*;
 import HollowKnight.view.elements.ElementViewer;
 import HollowKnight.view.elements.PairList;
 import HollowKnight.view.elements.ParticleViewer;
+import HollowKnight.view.elements.knight.knightStates.*;
 import HollowKnight.view.sprites.Sprite;
 import com.googlecode.lanterna.TextColor;
 
@@ -18,131 +19,52 @@ import java.util.Map;
 
 public class KnightViewer implements ElementViewer<Knight> {
 
-    private final List<Sprite> idleSpriteRight;
-    private final List<Sprite> idleSpriteLeft;
-    private final List<Sprite> maxVelocityRight;
-    private final List<Sprite> maxVelocityLeft;
-    private final List<Sprite> runningSpriteRight;
-    private final List<Sprite> runningSpriteLeft;
-    private final List<Sprite> walkingSpriteRight;
-    private final List<Sprite> walkingSpriteLeft;
-    private final List<Sprite> fallingSpriteRight;
-    private final List<Sprite> fallingSpriteLeft;
-    private final List<Sprite> jumpingSpriteRight;
-    private final List<Sprite> jumpingSpriteLeft;
-    private final Map<Class<?>, PairList<Sprite>> spriteMap;
-    private final Map<Class<?>, Integer> animationFPSMap;
+    private final List<StateAnimation> animations;
+
     private final ParticleViewer particleViewer;
 
     public KnightViewer() throws IOException {
-        this.spriteMap = new HashMap<>();
-        this.animationFPSMap = new HashMap<>();
-
+        this.animations = new ArrayList<>();
         this.particleViewer = new ParticleViewer();
 
-        //IDLE
+        // Initialize each animation state
+        animations.add(new IdleAnimation(IdleState.class, 8));
+        animations.add(new MaxVelocityAnimation(MaxVelocityState.class, 10));
+        animations.add(new RunningAnimation(RunningState.class, 5));
+        animations.add(new WalkingAnimation(WalkingState.class, 5));
+        animations.add(new FallingAnimation(FallingState.class, 10));
+        animations.add(new JumpAnimation(JumpState.class, 6));
+        animations.add(new RespawnAnimation(RespawnState.class, 0)); // No animation
+        animations.add(new DashAnimation(DashState.class, 5));
+        animations.add(new AfterDashAnimation(AfterDashState.class, 5));
 
-        idleSpriteRight = new ArrayList<>();
-        idleSpriteLeft = new ArrayList<>();
-        for (int i = 0; i < 16; i++) {
-            idleSpriteRight.add(new Sprite("sprites/Knight/Idle/pixil-frame-" + i + ".png"));
-            idleSpriteLeft.add(new Sprite("sprites/Knight/Idle/pixil-frame-" + i + "-reversed.png"));
+        // Load animations for each state
+        for (StateAnimation animation : animations) {
+            animation.loadAnimation("sprites/Knight"); // Base path to sprites
         }
-        spriteMap.put(IdleState.class, new PairList<>(idleSpriteRight, idleSpriteLeft));
-        animationFPSMap.put(IdleState.class, 8); // 8 FPS for idle animation
 
-        // MAXVELOCITY
-
-        maxVelocityRight = new ArrayList<>();
-        maxVelocityLeft = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            maxVelocityRight.add(new Sprite("sprites/Knight/movement/maxVelocity/frame-" + i + ".png"));
-            maxVelocityLeft.add(new Sprite("sprites/Knight/movement/maxVelocity/frame-" + i + "-reversed.png"));
-        }
-        spriteMap.put(MaxVelocityState.class, new PairList<>(maxVelocityRight, maxVelocityLeft));
-        animationFPSMap.put(MaxVelocityState.class, 10); // 12 FPS for max velocity animation
-
-        //RUNNING
-
-        runningSpriteRight = new ArrayList<>();
-        runningSpriteLeft = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            runningSpriteRight.add(new Sprite("sprites/Knight/movement/running/running-final-" + i + ".png"));
-            runningSpriteLeft.add(new Sprite("sprites/Knight/movement/running/running-final-" + i + "-reversed.png"));
-        }
-        spriteMap.put(RunningState.class, new PairList<>(runningSpriteRight, runningSpriteLeft));
-        animationFPSMap.put(RunningState.class, 5); // 10 FPS for running animation
-
-        // WALKING
-
-        walkingSpriteRight = new ArrayList<>();
-        walkingSpriteLeft = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            walkingSpriteLeft.add(new Sprite("sprites/Knight/movement/walking/running-intermediate-" + i + ".png"));
-            walkingSpriteRight.add(new Sprite("sprites/Knight/movement/walking/running-intermediate-" + i + "-reversed.png"));
-        }
-        spriteMap.put(WalkingState.class, new PairList<>(walkingSpriteLeft, walkingSpriteRight));
-        animationFPSMap.put(WalkingState.class, 5); // 6 FPS for walking animation
-
-        // FALLING
-
-        fallingSpriteRight = new ArrayList<>();
-        fallingSpriteLeft = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            fallingSpriteLeft.add(new Sprite("sprites/Knight/movement/falling/pixil-frame-" + i +".png"));
-            fallingSpriteRight.add(new Sprite("sprites/Knight/movement/falling/pixil-frame-"+ i + "-reversed.png"));
-        }
-        spriteMap.put(FallingState.class, new PairList<>(fallingSpriteLeft, fallingSpriteRight));
-        animationFPSMap.put(FallingState.class, 10);
-
-        // JUMPING
-
-        jumpingSpriteLeft = new ArrayList<>();
-        jumpingSpriteRight = new ArrayList<>();
-
-        for (int i = 0; i < 1; i++) {
-            jumpingSpriteLeft.add(new Sprite("sprites/Knight/movement/jumping/pixil-frame-" + i + ".png"));
-            jumpingSpriteRight.add(new Sprite("sprites/Knight/movement/jumping/pixil-frame-" + i + "-reversed.png"));
-        }
-        spriteMap.put(JumpState.class, new PairList<>(jumpingSpriteLeft, jumpingSpriteRight));
-        animationFPSMap.put(JumpState.class, 6);
-
-        spriteMap.put(RespawnState.class, new PairList<>(null, null));
-        animationFPSMap.put(RespawnState.class, 0);
-
-        spriteMap.put(DashState.class, new PairList<>(maxVelocityLeft, maxVelocityRight));
-        animationFPSMap.put(DashState.class, 5);
-
-        spriteMap.put(AfterDashState.class, new PairList<>(maxVelocityLeft, maxVelocityRight));
-        animationFPSMap.put(AfterDashState.class, 5);
     }
 
     @Override
     public void draw(Knight model, GUI gui, long time) throws IOException {
-        Sprite sprite = getSprite(time, model);
-        int offSetX = 4;
-        int offSetY = 1;
-        if (sprite != null)
-            sprite.draw(gui, (int) model.getPosition().x() - offSetX, (int) model.getPosition().y() - offSetY);
-        //gui.drawPixel((int)model.getPosition().x(), (int)model.getPosition().y(), new TextColor.RGB(0, 0, 0));
-
-        //gui.drawHitBox((int) model.getPosition().x(), (int) model.getPosition().y(), 7, 9, new TextColor.RGB(50, 200, 50));
-        //gui.drawPixel((int) model.getPosition().x(), (int) model.getPosition().y(), new TextColor.RGB(200, 200, 200));
+        StateAnimation animation = findAnimationForState(model.getState().getClass());
+        if (animation != null) {
+            Sprite sprite = animation.getSprite(animation, time, model.isFacingRight());
+            if (sprite != null) {
+                int offSetX = 4;
+                int offSetY = 1;
+                sprite.draw(gui, (int) model.getPosition().x() - offSetX, (int) model.getPosition().y() - offSetY);
+            }
+        }
     }
 
-    private Sprite getSprite(long tick, Knight model) {
-       /* if (model.getState().getClass() != IdleState.class) {
-            System.out.println("Knight current state: " + model.getState().getClass().getSimpleName());
-            System.out.println("Knight position: " + model.getPosition());
-            System.out.println("Knight velocity: " + model.getVelocity());
-        }*/
-        if (animationFPSMap.get(model.getState().getClass()) == 0)
-            return null;
-        int animationFPS = animationFPSMap.get(model.getState().getClass());
-        int animationFrameTime = 30 / animationFPS; // Assuming 60 ticks per second
-        PairList<Sprite> animations = spriteMap.get(model.getState().getClass());
-        List<Sprite> animationsDirection = model.isFacingRight() ? animations.getFirstList() : animations.getSecondList();
-        int frameIndex = (int) ((tick / animationFrameTime) % animationsDirection.size());
-        return animationsDirection.get(frameIndex);
+    private StateAnimation findAnimationForState(Class<?> stateClass) {
+        for (StateAnimation animation : animations) {
+            if (animation.getState() == stateClass) {
+                return animation;
+            }
+        }
+        return null;
     }
+
 }
