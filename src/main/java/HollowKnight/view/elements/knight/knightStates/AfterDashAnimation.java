7926181369eca6080725bf254d4a1ad2dda29dff
Freitas <adavidaf2002@gmail.com>
2024-11-26 12:04:15 +0000

    DashStates implementation for state pattern

---
+++ src/main/java/HollowKnight/view/elements/knight/knightStates/AfterDashAnimation.java
@@ -0,0 +1,18 @@
+package HollowKnight.view.elements.knight.knightStates;
+
+import HollowKnight.view.elements.PairList;
+
+import java.io.IOException;
+
+public class AfterDashAnimation extends StateAnimation{
+    public AfterDashAnimation(Class state, int frames) {
+        super(state, frames);
+    }
+
+    @Override
+    protected void loadAnimation(String path) throws IOException {
+        setState(AfterDashAnimation.class);
+        setAnimation(new PairList<>(null, null));
+        setFrames(1);
+    }
+}
