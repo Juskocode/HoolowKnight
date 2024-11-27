d9ea22c0ac8a435c69b27fbeabfea8cd194e183e
juskocode <adavidaf2002@gmail.com>
2024-11-27 04:50:12 +0000

    Implemented respective states for the menu particle

---
+++ src/main/java/HollowKnight/state/particle/WindyState.java
@@ -0,0 +1,18 @@
+package HollowKnight.state.particle;
+
+import HollowKnight.controller.menu.ParticleMenuController;
+import HollowKnight.model.Position;
+import HollowKnight.model.menu.Particle;
+
+public class WindyState implements ParticleState {
+    @Override
+    public Position move(Particle particle, long tick, ParticleMenuController controller) {
+        double windAngle = controller.getWindAngle(); // Obtain wind properties from the controller
+        double windSpeed = controller.getWindSpeed();
+
+        int newX = (int) (particle.getPosition().x() + (int) (windSpeed * Math.cos(windAngle)));
+        int newY = (int) (particle.getPosition().y() + (int) (windSpeed * Math.sin(windAngle)));
+
+        return controller.wrapPosition(newX, newY);
+    }
+}
