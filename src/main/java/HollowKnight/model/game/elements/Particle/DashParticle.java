02d5e9a08405c8b184adc9482fa493109e32ca6e
frutastontas <joaopedrofurtado2005@gmail.com>
2024-12-08 15:30:30 +0000

    Dash particle changes

--- src/main/java/HollowKnight/model/game/elements/Particle/DashParticle.java
+++ src/main/java/HollowKnight/model/game/elements/Particle/DashParticle.java
@@ -43,15 +43,13 @@ public class DashParticle extends Particle{
                 getPosition().y() + getVelocity().y()
         );
 
-        // Apply gravity to y-velocity
-        Position velocity = new Position(getVelocity().x(), getVelocity().y() + 0.1);
+        Position velocity = new Position(
+                getVelocity().x(),
+                getVelocity().y() + 0.2
+        );
         this.setVelocity(velocity);
-        // Reduce opacity to fade out
-        this.setOpacity(Math.max(0, getOpacity() - getFadeRate() * getVelocity().y()));
 
-
-
-        return new Position(position.x() + velocity.x(),
+        return  new Position(position.x() + velocity.x(),
                 position.y() + velocity.y());
     }
 }
