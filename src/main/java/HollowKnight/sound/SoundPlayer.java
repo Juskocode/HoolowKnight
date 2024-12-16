2ad8558c50b439e0215ecd6c4f84613fd36253e7
frutastontas <joaopedrofurtado2005@gmail.com>
2024-12-16 21:10:51 +0000

    new SoundPlayer Interface

---
+++ src/main/java/HollowKnight/sound/SoundPlayer.java
@@ -0,0 +1,11 @@
+package HollowKnight.sound;
+
+import javax.sound.sampled.Clip;
+
+public interface SoundPlayer {
+    void start();
+    void stop();
+
+    void setSound(Clip sound);
+    Clip getSound();
+}
