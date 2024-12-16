14318de1410acaaed0dfc7436dd5fe1d4c890c1d
Freitas <adavidaf2002@gmail.com>
2024-12-16 23:58:40 +0000

    OutofBounds fix

--- src/main/java/HollowKnight/model/game/scene/Scene.java
+++ src/main/java/HollowKnight/model/game/scene/Scene.java
@@ -197,8 +197,7 @@ public class Scene {
     }
 
     private boolean isOutSideScene(double x1, double x2, double y1, double y2) {
-        return false;
-        //return x1 < 0 || x2 >= this.width || y1 < 0 || y2 >= this.height;
+        return x1 < 0 || x2 >= this.width || y1 < 0 || y2 >= this.height;
     }
 
     private boolean checkCollision(double x1, double x2, double y1, double y2, Element[][] layer) {
