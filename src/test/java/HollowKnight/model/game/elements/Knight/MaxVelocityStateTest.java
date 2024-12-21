986ea5d83c58bb4c3e313da80fcd35e714116878
frutastontas <joaopedrofurtado2005@gmail.com>
2024-12-21 18:27:58 +0000

    New tests

---
+++ src/test/java/HollowKnight/model/game/elements/Knight/MaxVelocityStateTest.java
@@ -0,0 +1,99 @@
+package HollowKnight.model.game.elements.Knight;
+
+import HollowKnight.model.Vector;
+import HollowKnight.model.game.scene.Scene;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class MaxVelocityStateTest {
+    private Knight knight;
+    private MaxVelocityState maxVelocityState;
+    private Scene mockedScene;
+
+    @BeforeEach
+    void setUp() {
+        this.mockedScene = Mockito.mock(Scene.class);
+        knight = new Knight(0, 0, 0,0,0);
+        this.maxVelocityState = new MaxVelocityState(knight);
+        knight.setState(maxVelocityState);
+        knight.setVelocity(new Vector(1.8, 0));
+        knight.setScene(mockedScene);
+    }
+
+    @Test
+    void jump() {
+        Vector result = maxVelocityState.jump();
+        assertEquals(-Math.PI, result.y());
+    }
+
+    @Test
+    void dashRight() {
+        knight.setFacingRight(true);
+        knight.setVelocity(new Vector(1, 0));
+        Vector result = maxVelocityState.dash();
+
+        assertEquals(7.0, result.x());
+        assertTrue(knight.isFacingRight());
+    }
+
+    @Test
+    void dashLeft() {
+        knight.setFacingRight(false);
+        knight.setVelocity(new Vector(-1, 0));
+        Vector result = maxVelocityState.dash();
+
+        assertEquals(-7.0, result.x());
+        assertFalse(knight.isFacingRight());
+    }
+
+    @Test
+    void updateVelocity() {
+        when(mockedScene.collidesRight(Mockito.any(), Mockito.any())).thenReturn(false);
+        knight.setVelocity(new Vector(1, 0));
+        Vector result = maxVelocityState.updateVelocity(knight.getVelocity());
+        assertEquals(0.75, result.x());
+        assertEquals(0.0, result.y());
+    }
+
+    @Test
+    void getNextStateStay() {
+        when(mockedScene.collidesDown(Mockito.any(), Mockito.any())).thenReturn(true);
+        KnightState nextState = maxVelocityState.getNextState();
+        assertInstanceOf(RunningState.class, nextState);
+    }
+
+    @Test
+    void getNextStateRunning() {
+        when(mockedScene.collidesDown(Mockito.any(), Mockito.any())).thenReturn(true);
+        knight.setVelocity(new Vector(1.5, 0));
+
+        KnightState nextState = maxVelocityState.getNextState();
+
+        assertInstanceOf(RunningState.class, nextState);
+    }
+
+    @Test
+    void getNextStateDashing() {
+        when(knight.isOnGround()).thenReturn(true);
+        knight.setVelocity(new Vector(10, knight.getVelocity().y()));
+
+        KnightState nextState = maxVelocityState.getNextState();
+
+        assertInstanceOf(DashState.class, nextState);
+    }
+
+    @Test
+    void getNextStateJumping() {
+        when(knight.isOnGround()).thenReturn(false);
+        knight.setVelocity(new Vector(knight.getVelocity().x(), -10));
+
+        KnightState nextState = maxVelocityState.getNextState();
+
+        assertInstanceOf(JumpState.class, nextState);
+    }
+
+}
---
+++ src/test/java/HollowKnight/model/game/elements/Knight/RespawnStateTest.java
@@ -0,0 +1,80 @@
+package HollowKnight.model.game.elements.Knight;
+
+import HollowKnight.model.Position;
+import HollowKnight.model.Vector;
+import HollowKnight.model.game.elements.Particle.Particle;
+import HollowKnight.model.game.elements.Particle.RespawnParticle;
+import HollowKnight.model.game.scene.Scene;
+import HollowKnight.view.elements.PairList;
+import com.googlecode.lanterna.TextColor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+class RespawnStateTest {
+    private Knight knight;
+    private RespawnState respawnState;
+    private Scene mockedScene;
+    private List<Particle> respawnParticles;
+
+    @BeforeEach
+    void setUp() {
+        this.mockedScene = Mockito.mock(Scene.class);
+        this.knight = new Knight(0, 0, 0,0,0);
+        this.respawnState = new RespawnState(knight,10);
+        this.knight.setState(respawnState);
+        this.knight.setVelocity(new Vector(0, 0));
+        this.knight.setScene(mockedScene);
+        this.respawnParticles = knight.createRespawnParticles(450);
+        doAnswer(invocation -> {
+            ArrayList<Particle> input = invocation.getArgument(0);
+            input.add(new RespawnParticle(0,0,new Position(0,0), new TextColor.RGB(0,0,0)));
+            return null;
+        }).when(mockedScene).setRespawnParticles(any(ArrayList.class));
+    }
+
+    /*@Test
+    void jump() {
+        Vector result = respawnState.jump();
+
+        assertEquals(0, result.x());
+        assertEquals(0, result.y());
+    }
+
+    @Test
+    void dash() {
+        Vector result = respawnState.dash();
+
+        assertEquals(0, result.x());
+        assertEquals(0, result.y());
+        assertTrue(knight.isFacingRight());
+    }
+
+    @Test
+    void updateVelocity() {
+        Vector result = respawnState.updateVelocity(knight.getVelocity());
+
+        assertEquals(0, result.x());
+        assertEquals(0, result.y());
+    }
+
+    @Test
+    void getNextState() {
+
+    }
+
+    @Test
+    void getNextStateStay() {
+        KnightState nextState = respawnState.getNextState();
+
+        assertSame(respawnState, nextState);
+    }*/
+}
