c2dacfe8bc73d52f8f4310b282cbba078cc0bab7
frutastontas <joaopedrofurtado2005@gmail.com>
2024-12-22 19:39:27 +0000

    Eleminate attack

---
+++ src/test/java/HollowKnight/model/game/elements/Particle/DashParticleTest.java
@@ -0,0 +1,31 @@
+package HollowKnight.model.game.elements.Particle;
+
+import HollowKnight.model.Position;
+import HollowKnight.model.game.scene.Scene;
+import com.googlecode.lanterna.TextColor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DashParticleTest {
+    private Scene scene;
+    private int time;
+    private DashParticle dashParticle;
+    @BeforeEach
+    void setUp() {
+        this.scene = Mockito.mock(Scene.class);
+        this.time=0;
+        this.dashParticle = new DashParticle(1,1, new Position(1,1)
+                ,new TextColor.RGB(0,0,0));
+    }
+
+    @Test
+    void moveParticle() {
+        Position position = dashParticle.moveParticle(scene,time);
+        Assertions.assertNotEquals(position.x(), dashParticle.getPosition().x());
+        Assertions.assertNotEquals(position.y(), dashParticle.getPosition().y());
+    }
+}
