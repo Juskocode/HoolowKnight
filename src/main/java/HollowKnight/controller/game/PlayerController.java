b134b89a0f380c631fd6d07d9654b605373197db
Freitas <adavidaf2002@gmail.com>
2024-12-22 19:48:24 +0000

    remove comments add fixed exception

--- src/main/java/HollowKnight/controller/game/PlayerController.java
+++ src/main/java/HollowKnight/controller/game/PlayerController.java
@@ -8,13 +8,15 @@ import HollowKnight.model.game.elements.Knight.Knight;
 import HollowKnight.model.game.elements.Knight.RespawnState;
 import HollowKnight.model.game.scene.Scene;
 
+import java.io.IOException;
+
 public class PlayerController extends Controller<Scene> {
     public PlayerController(Scene scene) {
         super(scene);
     }
 
     @Override
-    public void move(Game game, GUI.ACTION action, long time) {
+    public void move(Game game, GUI.ACTION action, long time) throws IOException {
         Knight knight = getModel().getPlayer();
 
         switch (action) {
@@ -27,21 +29,6 @@ public class PlayerController extends Controller<Scene> {
                 knight.setFacingRight(true);
                 break;
             case JUMP:
-                /*
-                if (gui.isJumpHeld()) { // Process jump only if the key is being held
-
-                    Vector velocity = knight.jump();
-                    long duration = gui.getDuration();
-
-                    // Clamp duration between 80 and 250
-                    if (duration > 250) duration = 250;
-                    else if (duration < 80) duration = 80;
-
-                    double jumpMultiplier = duration / 250.0;
-                    System.out.println("JumpBoost " + jumpMultiplier);
-
-                    knight.setVelocity(velocity);
-                }*/
                 knight.setVelocity(knight.jump());
                 break;
             case DASH:
