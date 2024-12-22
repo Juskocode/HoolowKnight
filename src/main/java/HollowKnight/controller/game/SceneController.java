196a6c62bb861685b54dff99d69fca2ea8445653
Freitas <adavidaf2002@gmail.com>
2024-12-22 01:31:18 +0000

    SceneController small bug fix regrading menustate

--- src/main/java/HollowKnight/controller/game/SceneController.java
+++ src/main/java/HollowKnight/controller/game/SceneController.java
@@ -6,9 +6,9 @@ import HollowKnight.gui.GUI;
 import HollowKnight.model.game.elements.Knight.Knight;
 import HollowKnight.model.game.scene.Scene;
 import HollowKnight.model.game.scene.SceneLoader;
-import HollowKnight.model.menu.Menu;
+import HollowKnight.model.menu.MainMenu;
 import HollowKnight.state.GameState;
-import HollowKnight.state.MenuState;
+import HollowKnight.state.MainMenuState;
 
 import java.io.IOException;
 
@@ -28,7 +28,7 @@ public class SceneController extends Controller<Scene> {
     public void move(Game game, GUI.ACTION action, long time) throws IOException {
         Knight knight = getModel().getPlayer();
         if (action == GUI.ACTION.QUIT)
-            game.setState(new MenuState(new Menu()));
+            game.setState(new MainMenuState(new MainMenu()));
         else {
 
             playerController.move(game, action, time);
@@ -38,7 +38,7 @@ public class SceneController extends Controller<Scene> {
                     //Credits credits = new Credits(player);
                     //game.setState(new CreditsState(credits, game.getSpriteLoader()));
                     //if (getModel().isAtEndPosition())
-                    game.setState(new MenuState(new Menu()));
+                    game.setState(new MainMenuState(new MainMenu()));
 
                 } else {
                     SceneLoader sceneLoader = new SceneLoader((getModel().getSceneID() + 1));
