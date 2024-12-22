bb32333fb777ce3dbce068538dc14513f7aa8afb
frutastontas <joaopedrofurtado2005@gmail.com>
2024-12-22 19:39:42 +0000

    Eleminate attack

--- src/main/java/HollowKnight/model/game/elements/attacks/SR_Attack.java
+++
@@ -1,8 +0,0 @@
-package HollowKnight.model.game.elements.attacks;
-//ataques "Short range"
-public class SR_Attack extends Attack{
-
-    public SR_Attack(int hp, int energy_loss, int range) {
-        super(hp, energy_loss, range);
-    }
-}
