75c5010b5d6c37bcfe8a4a8731cf9e149ef59a93
Freitas <adavidaf2002@gmail.com>
2024-12-22 01:27:11 +0000

    Added GameIcon to MainMenu

--- src/main/java/HollowKnight/state/State.java
+++ src/main/java/HollowKnight/state/State.java
@@ -5,7 +5,9 @@ import HollowKnight.controller.Controller;
 import HollowKnight.gui.GUI;
 import HollowKnight.view.states.ScreenViewer;
 
+import java.awt.*;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public abstract class State<T> {
     private final T model;
@@ -14,18 +16,17 @@ public abstract class State<T> {
 
     public State(T model) throws IOException {
         this.model = model;
-        this.screenViewer = getScreenViewer();
-        this.controller = getController();
+        this.screenViewer = createScreenViewer();
+        this.controller = createController();
     }
 
-    protected abstract ScreenViewer<T> getScreenViewer() throws IOException;
-    protected abstract Controller<T> getController();
-
+    protected abstract ScreenViewer<T> createScreenViewer() throws IOException;
+    protected abstract Controller<T> createController();
     public T getModel() {
         return model;
     }
 
-    public void move(Game game, GUI gui, long time) throws IOException {
+    public void move(Game game, GUI gui, long time) throws IOException, URISyntaxException, FontFormatException {
         GUI.ACTION action = gui.getACTION();
         controller.move(game, action, time);
         screenViewer.draw(gui, time);
