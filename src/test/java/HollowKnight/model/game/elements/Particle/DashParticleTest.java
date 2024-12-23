5b380238d8005abe8c506cd28ebe62208b6c1e73
frutastontas <joaopedrofurtado2005@gmail.com>
2024-12-23 17:40:27 +0000

    new PBT in particles

--- src/test/java/HollowKnight/model/game/elements/Particle/DashParticleTest.java
+++ src/test/java/HollowKnight/model/game/elements/Particle/DashParticleTest.java
@@ -3,11 +3,16 @@ package HollowKnight.model.game.elements.Particle;
 import HollowKnight.model.Position;
 import HollowKnight.model.game.scene.Scene;
 import com.googlecode.lanterna.TextColor;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static java.awt.image.ImageObserver.HEIGHT;
+import static java.awt.image.ImageObserver.WIDTH;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DashParticleTest {
@@ -27,5 +32,17 @@ class DashParticleTest {
         Position position = dashParticle.moveParticle(scene,time);
         Assertions.assertNotEquals(position.x(), dashParticle.getPosition().x());
         Assertions.assertNotEquals(position.y(), dashParticle.getPosition().y());
+    }
+
+    @Property
+    public void move(
+            @ForAll @IntRange(max = WIDTH) int x,
+            @ForAll @IntRange(max = HEIGHT) int y
+    ) {
+        DashParticle dashParticle1 = new DashParticle(x, y, new Position(0,0), new TextColor.RGB(0,0,0));
+
+        Position newPosition = dashParticle1.moveParticle(scene,25);
+
+        assertTrue((newPosition.x() != dashParticle1.getPosition().x()) || newPosition.y() != dashParticle1.getPosition().y());
     }
 }
