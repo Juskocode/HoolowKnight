fb4396de622dbd24ef31609853f19c6670c2aacb
Freitas <adavidaf2002@gmail.com>
2024-12-23 23:14:37 +0000

    Refactored dsa into model struct

--- src/main/java/HollowKnight/model/game/elements/Particle/JumpParticle.java
+++ src/main/java/HollowKnight/model/game/elements/Particle/JumpParticle.java
@@ -1,7 +1,7 @@
 package HollowKnight.model.game.elements.Particle;
 
-import HollowKnight.model.Position;
-import HollowKnight.model.Vector;
+import HollowKnight.model.dataStructs.Position;
+import HollowKnight.model.dataStructs.Vector;
 import HollowKnight.model.game.scene.Scene;
 import com.googlecode.lanterna.TextColor;
 
