f913593494d10064949b92351c1178c10a9dda5c
Freitas <adavidaf2002@gmail.com>
2024-12-23 23:18:29 +0000

    removed unused trees classes

--- src/main/java/HollowKnight/model/game/elements/Tree/SmallTree.java
+++
@@ -1,9 +0,0 @@
-package HollowKnight.model.game.elements.Tree;
-
-import HollowKnight.model.game.elements.Element;
-
-public class SmallTree extends Element {
-    public SmallTree(int x, int y) {
-        super(x, y);
-    }
-}
