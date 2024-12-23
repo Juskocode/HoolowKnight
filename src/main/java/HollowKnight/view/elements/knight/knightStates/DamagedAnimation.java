04bafbd0f0edb9cbc74bae8621b26fcaa9cd1ed6
Freitas <adavidaf2002@gmail.com>
2024-12-23 23:14:00 +0000

    Refactored dsa into model struct

--- src/main/java/HollowKnight/view/elements/knight/knightStates/DamagedAnimation.java
+++ src/main/java/HollowKnight/view/elements/knight/knightStates/DamagedAnimation.java
@@ -1,8 +1,7 @@
 package HollowKnight.view.elements.knight.knightStates;
 
 import HollowKnight.model.game.elements.Knight.DamagedState;
-import HollowKnight.model.game.elements.Knight.MaxVelocityState;
-import HollowKnight.view.elements.PairList;
+import HollowKnight.model.dataStructs.PairList;
 import HollowKnight.view.sprites.Sprite;
 
 import java.io.IOException;
