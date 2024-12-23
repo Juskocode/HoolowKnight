6aa0da8a19136063ba7bb4c89313c80a54d4da68
Freitas <adavidaf2002@gmail.com>
2024-12-23 23:14:14 +0000

    Refactored dsa into model struct

--- src/test/java/HollowKnight/model/game/elements/Particle/DoubleJumpParticleTest.java
+++ src/test/java/HollowKnight/model/game/elements/Particle/DoubleJumpParticleTest.java
@@ -1,14 +1,12 @@
 package HollowKnight.model.game.elements.Particle;
 
-import HollowKnight.model.Position;
+import HollowKnight.model.dataStructs.Position;
 import HollowKnight.model.game.scene.Scene;
 import com.googlecode.lanterna.TextColor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleJumpParticleTest {
     private Scene scene;
